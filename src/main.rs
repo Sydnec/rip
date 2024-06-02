93fe2b9c368d4e00627faa43024e70a8c27f36eb
Sydnec <>
2024-06-02 17:44:49 +0200

    Passage sur PC fixe

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 mod lecture_config;
+mod routing;
 
 use lecture_config::read_config_files;
+use routing::RoutingTable;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let config_dir = "./";
+    let config_dir = "./config";
     let pattern = format!("{}/*.yaml", config_dir);
     let interfaces = read_config_files(&pattern)?;
 
+    let mut routing_table = RoutingTable::new();
     for interface in interfaces {
-        println!("{:?}", interface);
+        routing_table.add_interface(interface);
     }
+
+    routing_table.display();
 
     Ok(())
 }
